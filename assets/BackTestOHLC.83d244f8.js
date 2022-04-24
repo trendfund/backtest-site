37c0764028f1752e7969349dc158066dca44911a
anoymouse <anoymouse@x.com>
2022-04-24 10:08:46 +0000

    update

--- assets/BackTestOHLC.83d244f8.js
+++ assets/BackTestOHLC.83d244f8.js
@@ -1,4 +1,4 @@
-import{f7 as Br,fj as Fr,r as zr,f8 as ni,o as Vr,ff as jr,a as Ct,e as Hr,b as Yt,c as he,w as at,d as Ot,f as ii,u as Dt,fp as Wt,t as Se,g as ri,i as ai,F as oi,fw as Nr,fx as Yr,fn as Wr,j as Xr}from"./index.eb7b0533.js";import{c as Gr,o as Ur}from"./index.7351ec41.js";var Ze={exports:{}},Ke={exports:{}};/**
+import{f7 as Br,fj as Fr,r as zr,f8 as ni,o as Vr,ff as jr,a as Ct,e as Hr,b as Yt,c as he,w as at,d as Ot,f as ii,u as Dt,fp as Wt,t as Se,g as ri,i as ai,F as oi,fw as Nr,fx as Yr,fn as Wr,j as Xr}from"./index.e6768ac2.js";import{c as Gr,o as Ur}from"./index.80adeed2.js";var Ze={exports:{}},Ke={exports:{}};/**
  * @license
  * KLineChart v8.3.6
  * Copyright (c) 2019 lihu.
